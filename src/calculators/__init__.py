6139df54937f31d7a2064d82af13fbb666047a44
kyusongy <kyusongy@unc.edu>
2026-01-05 13:20:03 +0800

    Manual input initial implementation

--- src/calculators/__init__.py
+++ src/calculators/__init__.py
@@ -2,6 +2,13 @@
 from .income import calculate_income
 from .advance_settlement import calculate_advance_settlement
 from .nature import NatureMapper
+from .manual_input import ManualInputProcessor, mark_processed_groups
 
-__all__ = ['calculate_income', 'calculate_advance_settlement', 'NatureMapper']
+__all__ = [
+    'calculate_income', 
+    'calculate_advance_settlement', 
+    'NatureMapper',
+    'ManualInputProcessor',
+    'mark_processed_groups',
+]
 
